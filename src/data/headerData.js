e24df0de5b79e67585b02a91d85fe5f360471a7b
prarit22 <praritmehta223@gmail.com>
2023-08-28 23:15:54 +0530

    main

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,6 +3,6 @@ export const headerData = {
     name: 'PRARIT MEHTA',
     title: "Hello! I am",
     desciption: "Hello! I am PRARIT MEHTA, a Cybersecurity in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'src/assets/svg/mehta.png.svg',
+    image: 'src/assets/png/mehtadp.png',
     resumePdf: 'https://drive.google.com/drive/folders/1-K_K-pcQGlPigOvOoHHQtcBdBSF2oBKK'
 }
