8a31eb75389a256ad6a8a1c8f45c7550ec33e321
prarit22 <praritmehta223@gmail.com>
2023-08-28 23:08:14 +0530

    main

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,6 @@ export const headerData = {
     name: 'PRARIT MEHTA',
     title: "Hello! I am",
     desciption: "Hello! I am PRARIT MEHTA, a Cybersecurity in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'https://i.ibb.co/ZmdNH6x/abu-said.jpg',
-    imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
+    image: 'src/assets/svg/mehta.png.svg',
     resumePdf: 'https://drive.google.com/drive/folders/1-K_K-pcQGlPigOvOoHHQtcBdBSF2oBKK'
 }
